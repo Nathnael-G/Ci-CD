9e8f5f5107de55e88811fa24d45268144518cf27
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-01-25 11:01:12 +0300

    commit greeting app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, there!"
+@app.route("/", methods=["GET", "POST"])
+def home():
+    greeting = ""
+    if request.method == "POST":
+        name = request.form.get("name")
+        if name:
+            greeting = f"Hello, {name}!"
+    return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
     app.run(debug=True)
