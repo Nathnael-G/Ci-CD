ec0a0d4419933f6976ef82889d0bac798fa2ec7c
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-01-27 10:55:43 +0300

    Modified app for anti-clickjacking

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.after_request
+def set_x_frame_options(response):
+    """Set X-Frame-Options header to prevent clickjacking."""
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'  # Change to 'DENY' if you want to disallow all framing
+    return response
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     """Home route that handles GET and POST requests."""
